fc4d7138e45fa3406df85ca60e72f3d2ce46935d
filip <sh333lby@gmail.com>
2019-05-11 17:25:54 +0200

    working

--- remove.py
+++ remove.py
@@ -8,13 +8,16 @@ imapObj = imapclient.IMAPClient('poczta.o2.pl', ssl=True)
 imapObj.login(config.username, config.password)
 
 # pprint.pprint(imapObj.list_folders())
-imapObj.select_folder('INBOX', readonly=True)
+imapObj.select_folder('INBOX', readonly=False)
 # UIDs = imapObj.search(['ALL'])
 # print(UIDs)
 
 messages = imapObj.search('UNSEEN')
+adressReg = re.compile(r"(<o2@o2.pl>|<no-response@o2.pl>)")
 
 for uid, message_data in imapObj.fetch(messages, 'RFC822').items():
     email_message = email.message_from_bytes(message_data[b'RFC822'])
+    if re.search(adressReg, email_message.get('From')) is not None:
+        imapObj.delete_messages(uid)
 
-    print(email_message.get('From'))
+imapObj.logout()
