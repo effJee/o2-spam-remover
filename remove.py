d770ca69b0657498137c2c8fece715313f53f424
filip <sh333lby@gmail.com>
2019-05-11 23:55:53 +0200

    uncommented

--- remove.py
+++ remove.py
@@ -9,18 +9,18 @@ print("something")
 imapObj = imapclient.IMAPClient('poczta.o2.pl', ssl=True, timeout=5)
 print("after")
 imapObj.login(environ.get('USERNAME_KEY'), environ.get('PASSWORD_KEY'))
-#
-# # pprint.pprint(imapObj.list_folders())
-# imapObj.select_folder('INBOX', readonly=False)
-# # UIDs = imapObj.search(['ALL'])
-# # print(UIDs)
-#
-# messages = imapObj.search('UNSEEN')
-# adresReg = re.compile(r"(<o2@o2.pl>|<no-response@o2.pl>)")
-#
-# for uid, message_data in imapObj.fetch(messages, 'RFC822').items():
-#     email_message = email.message_from_bytes(message_data[b'RFC822'])
-#     if re.search(adresReg, email_message.get('From')) is not None:
-#         imapObj.delete_messages(uid)
-#
-# imapObj.logout()
+
+# pprint.pprint(imapObj.list_folders())
+imapObj.select_folder('INBOX', readonly=False)
+# UIDs = imapObj.search(['ALL'])
+# print(UIDs)
+
+messages = imapObj.search('UNSEEN')
+adresReg = re.compile(r"(<o2@o2.pl>|<no-response@o2.pl>)")
+
+for uid, message_data in imapObj.fetch(messages, 'RFC822').items():
+    email_message = email.message_from_bytes(message_data[b'RFC822'])
+    if re.search(adresReg, email_message.get('From')) is not None:
+        imapObj.delete_messages(uid)
+
+imapObj.logout()
