05f3e5b38c50d57d217d94f2a84b34eb6de30391
filip <sh333lby@gmail.com>
2019-05-11 22:37:50 +0200

    test

--- remove.py
+++ remove.py
@@ -5,7 +5,7 @@ import re
 import os
 
 print("something")
-imapObj = imapclient.IMAPClient('poczta.o2.pl', ssl=True, timeout=None)
+imapObj = imapclient.IMAPClient('poczta.o2.pl', ssl=True, timeout=100)
 print("after")
 # imapObj.login(os.environ.get['USERNAME_KEY'], os.environ.get['PASSWORD_KEY'])
 #
