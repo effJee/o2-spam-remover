78069eec04714c9d13ecc581e38f1e9cdf5c611f
filip <sh333lby@gmail.com>
2019-05-14 18:28:34 +0200

    gmail imap

--- remove.py
+++ remove.py
@@ -11,7 +11,7 @@ timeouts = 1
 while not connected:
     try:
         print("Connecting...")
-        imapObj = imapclient.IMAPClient('poczta.o2.pl',port=993, ssl=True, timeout=60)
+        imapObj = imapclient.IMAPClient('imap.gmail.com',port=993, ssl=True, timeout=35)
         print("Connected")
         imapObj.login(environ.get('USERNAME_KEY'), environ.get('PASSWORD_KEY'))
         print("Logged in")
@@ -20,19 +20,19 @@ while not connected:
         print("{}. Timed out".format(timeouts))
         timeouts += 1
 
-# pprint.pprint(imapObj.list_folders())
-imapObj.select_folder('INBOX', readonly=False)
-# UIDs = imapObj.search(['ALL'])
-# print(UIDs)
-
-messages = imapObj.search('UNSEEN')
-adresReg = re.compile(r"(<o2@o2.pl>|<no-response@o2.pl>)")
-
-for uid, message_data in imapObj.fetch(messages, 'RFC822').items():
-    email_message = email.message_from_bytes(message_data[b'RFC822'])
-    if re.search(adresReg, email_message.get('From')) is not None:
-        imapObj.delete_messages(uid)
-        print("Deleted")
+# # pprint.pprint(imapObj.list_folders())
+# imapObj.select_folder('INBOX', readonly=False)
+# # UIDs = imapObj.search(['ALL'])
+# # print(UIDs)
+#
+# messages = imapObj.search('UNSEEN')
+# adresReg = re.compile(r"(<o2@o2.pl>|<no-response@o2.pl>)")
+#
+# for uid, message_data in imapObj.fetch(messages, 'RFC822').items():
+#     email_message = email.message_from_bytes(message_data[b'RFC822'])
+#     if re.search(adresReg, email_message.get('From')) is not None:
+#         imapObj.delete_messages(uid)
+#         print("Deleted")
 
 imapObj.logout()
 print("Logged out")
